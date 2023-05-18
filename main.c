01cf3dc4a5094d656a4dd605eeb62c06ee1311d4
Ben Haubrich <bhaubrich@bourgault.com>
2023-05-17 21:18:35 -0600

    Remove commented out code from development.

--- main.c
+++ main.c
@@ -9,19 +9,19 @@ int main() {
   printf("Chip ID %x\r\n", dataRx);
 
   ramCommand(CMD_DLSTART);                   //Start a new display list
-//  ramCommand(VERTEXFORMAT(0));               //setup VERTEX2F to take pixel coordinates
-//  ramCommand(CLEAR_COLOR_RGB(0, 0, 0));      //Determine the clear screen color
-//  ramCommand(CLEAR(1, 1, 1));                //Clear the screen and the current display list
-//  ramCommand(COLOR_RGB(26, 26, 192));        // change colour to blue
-//  ramCommand(POINT_SIZE(30 * 16));      // set point size to DotSize pixels. Points = (pixels x 16)
-//  ramCommand(BEGIN(POINTS));                 // start drawing point
-//  ramCommand(TAG(1));                        // Tag the blue dot with a touch ID
-//  ramCommand(VERTEX2F(DWIDTH / 2, DHEIGHT / 2));     // place blue point
-//  ramCommand(END());                         // end drawing point
-//  ramCommand(COLOR_RGB(255, 255, 255));      //Change color to white for text
-//  //Cmd_Text(DWIDTH / 2, DHEIGHT / 2, 30, OPT_CENTER, " MATRIX         ORBITAL"); //Write text in the center of the screen
-//  ramCommand(DISPLAY());                     //End the display list
-//  ramCommand(CMD_SWAP);                      //Swap commands into RAM
-//  UpdateFIFO();                            // Trigger the CoProcessor to start processing the FIFO
+  ramCommand(VERTEXFORMAT(0));               //setup VERTEX2F to take pixel coordinates
+  ramCommand(CLEAR_COLOR_RGB(0, 0, 0));      //Determine the clear screen color
+  ramCommand(CLEAR(1, 1, 1));                //Clear the screen and the current display list
+  ramCommand(COLOR_RGB(26, 26, 192));        // change colour to blue
+  ramCommand(POINT_SIZE(30 * 16));      // set point size to DotSize pixels. Points = (pixels x 16)
+  ramCommand(BEGIN(POINTS));                 // start drawing point
+  ramCommand(TAG(1));                        // Tag the blue dot with a touch ID
+  ramCommand(VERTEX2F(DWIDTH / 2, DHEIGHT / 2));     // place blue point
+  ramCommand(END());                         // end drawing point
+  ramCommand(COLOR_RGB(255, 255, 255));      //Change color to white for text
+  //Cmd_Text(DWIDTH / 2, DHEIGHT / 2, 30, OPT_CENTER, " MATRIX         ORBITAL"); //Write text in the center of the screen
+  ramCommand(DISPLAY());                     //End the display list
+  ramCommand(CMD_SWAP);                      //Swap commands into RAM
+  UpdateFIFO();                            // Trigger the CoProcessor to start processing the FIFO
   exit(0);
 }
